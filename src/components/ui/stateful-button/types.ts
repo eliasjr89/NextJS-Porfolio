69e5a8fb58934f1cd14c234ca5b8123648a48919
eliasjr89 <eliasjiminian@gmail.com>
2025-09-05 18:41:40 +0200

    deleted duplicated files

--- src/components/ui/stateful-button/types.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  className?: string;
-  children: React.ReactNode;
-}
--- src/components/ui/vortex/helpers.ts
+++
@@ -1,7 +0,0 @@
-export const fadeInOut = (t: number, m: number): number => {
-  const hm = 0.5 * m;
-  return Math.abs(((t + hm) % m) - hm) / hm;
-};
-
-export const lerp = (n1: number, n2: number, speed: number): number =>
-  (1 - speed) * n1 + speed * n2;
